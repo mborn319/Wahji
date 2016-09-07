22f7c3a1b636d937fad0e2ecfcdb324e3708cfa0
Michael Born <mborn319@gmail.com>
2016-09-07 17:53:53 -0400

    UNTESTED markdown parsing, saving to output file

--- wahji.py
+++ wahji.py
@@ -3,11 +3,18 @@
 
 
 # FIRST: init the libraries we need for parsing Yaml, Markdown
+import markdown
 
-# Second: read the HTML files into a string
+# Second: read the HTML & Markdown files into a string
+htmlfile = open('template.md','r')
+mdfile = open('content.md','r')
 
 # Third: parse the markdown into an HTML string
+content = markdown.markdown(mdfile)
 
 # Fourth: replace the {content_here} blocks in the HTML with markdown
+html = html.replace('{content_here}',content)
 
 # Fifth: save the generated HTML into a new file.
+newfile = open('output/index.html','rw+')
+newfile.write(html)
