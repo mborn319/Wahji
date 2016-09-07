b003ddbc4d44c5b262047a106daedb57553dfc95
Michael Born <mborn319@gmail.com>
2016-09-07 17:01:25 -0400

    Initial state of build file

---
+++ wahji.py
@@ -0,0 +1,13 @@
+# this is the build script
+
+
+
+# FIRST: init the libraries we need for parsing Yaml, Markdown
+
+# Second: read the HTML files into a string
+
+# Third: parse the markdown into an HTML string
+
+# Fourth: replace the {content_here} blocks in the HTML with markdown
+
+# Fifth: save the generated HTML into a new file.
