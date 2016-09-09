8b848b3a09591b28808fd7bed51c9f51002966d6
Michael Born <mborn319@gmail.com>
2016-09-09 17:33:22 -0400

    Got Python-Markdown working

--- wahji.py
+++ wahji.py
@@ -1,20 +1,21 @@
 # this is the build script
 
 
-
 # FIRST: init the libraries we need for parsing Yaml, Markdown
 import markdown
 
 # Second: read the HTML & Markdown files into a string
-htmlfile = open('template.md','r')
+htmlfile = open('template.html','r')
+htmlfile = htmlfile.read().strip('.')
 mdfile = open('content.md','r')
+mdfile = mdfile.read().strip('.')
 
 # Third: parse the markdown into an HTML string
 content = markdown.markdown(mdfile)
 
 # Fourth: replace the {content_here} blocks in the HTML with markdown
-html = html.replace('{content_here}',content)
+html = htmlfile.replace('{content_here}',content)
 
 # Fifth: save the generated HTML into a new file.
-newfile = open('output/index.html','rw+')
+newfile = open('output/index.html','w')
 newfile.write(html)
