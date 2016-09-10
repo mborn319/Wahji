836d8f360d9ecb2c45820f4b889a25a4f98e0aa1
David Deeley <daviddeeley@Davids-MacBook-Pro.local>
2016-09-09 23:52:05 -0400

    Added function to check if .wahji file is present

--- wahji.py
+++ wahji.py
@@ -2,44 +2,60 @@
 
 import os, platform
 
-"""creates theme folder and its contents"""
-os.mkdir("content")
-os.mkdir("themes")
-os.mkdir("themes/css")
-os.mkdir("themes/img")
-os.mkdir("themes/js")
+def fname():
+	for root, dir, files in os.walk("."):
+	        if ".wahji" in files:
+	            print "Found it!"
+	            return True
+	            break
+	        else:
+	            print "Couldn't find .wahji."
+	            return False
+	            break
 
-"""creates hidden wahji file"""
-file = open(".wahji","w")
-file.close()
+boolean = fname()
 
-"""creates 404.html file"""
-file = open("404.html","w+")
-file.close()
+if boolean == False:
+	"""creates theme folder and its contents"""
+	os.mkdir("content")
+	os.mkdir("themes")
+	os.mkdir("themes/css")
+	os.mkdir("themes/img")
+	os.mkdir("themes/js")
 
-"""checks os to figure out where command directory will go"""
-my_platform = platform.system()
+	"""creates hidden wahji file"""
+	file = open(".wahji","w")
+	file.close()
 
-if my_platform == "CYGWIN_NT-6.1":
-	print "You're using CYGWIN_NT-6! Your command directory will be "
-	print "located in the current directory."
-	os.mkdir("commands")
+	"""creates 404.html file"""
+	file = open("404.html","w+")
+	file.close()
 
-elif my_platform == "Windows":
-	print "You're using Windows! Your command directory will be "
-	print "located in the current directory."
-	os.mkdir("commands")
+	"""checks os to figure out where command directory will go"""
+	my_platform = platform.system()
 
-elif my_platform == "Linux":
-	print "You're using Linux! Your command directory will be located in "
-	print "/usr/local/lib/python2.7/dist-packages/wahji/commands"
-	os.mkdir("/usr/local/lib/python2.7/dist-packages/wahji")
-	os.mkdir("/usr/local/lib/python2.7/dist-packages/wahji/commands")
+	if my_platform == "CYGWIN_NT-6.1":
+		print "You're using CYGWIN_NT-6! Your command directory will be "
+		print "located in the current directory."
+		os.mkdir("commands")
 
-elif my_platform == "Darwin":
-	print "You're using Mac OS! Your command directory will be  "
-	print "located in the current directory."
-	os.mkdir("commands")
+	elif my_platform == "Windows":
+		print "You're using Windows! Your command directory will be "
+		print "located in the current directory."
+		os.mkdir("commands")
 
+	elif my_platform == "Linux":
+		print "You're using Linux! Your command directory will be located in "
+		print "/usr/local/lib/python2.7/dist-packages/wahji/commands"
+		os.mkdir("/usr/local/lib/python2.7/dist-packages/wahji")
+		os.mkdir("/usr/local/lib/python2.7/dist-packages/wahji/commands")
+
+	elif my_platform == "Darwin":
+		print "You're using Mac OS! Your command directory will be  "
+		print "located in the current directory."
+		os.mkdir("commands")
+
+	else:
+		os.mkdir("commands")
 else:
-	os.mkdir("commands")
+	print "Wahji has already been initilized."
