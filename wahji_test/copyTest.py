05ca57164bdca5bc437df22779686caad5f0631e
happelj <happelj@sunyit.edu>
2016-09-13 01:18:36 -0400

    Made minor edits to yamlParser.py and removed wahji_test directory and contents

--- wahji_test/copyTest.py
+++
@@ -1,8 +0,0 @@
-import os
-
-directory = os.path.abspath("/home/ubuntu/CS370/wahji_test/content/")
-if os.path.exists(directory):
-    os.chdir(directory)
-else:
-    print "***" + directory + " does not exist***"
-print "The current directory is " + os.getcwd()
