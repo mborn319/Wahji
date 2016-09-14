a0de8bcff0a40bba78f065b5b692748de8fe9183
David Deeley <dadeeley67@gmail.com>
2016-09-14 17:54:19 -0400

    Cleaned up dev folder

--- parser.py
+++
@@ -1,9 +0,0 @@
-import markdown
-
-md = open("test.md","r+")
-md = md.read()
-md = markdown.markdown(md)
-
-html = open("test.html","w")
-html.write(md);
-html.close()
--- setup/setup.sh
+++
@@ -1,4 +0,0 @@
-echo python $PWD/wahji.py \$@ >> wahji.sh
-mv wahji.sh wahji #rename script file
-chmod +x wahji #make script file executable
-mv wahji /bin #move file to /bin
