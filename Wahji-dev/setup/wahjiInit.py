514741da230d272e54a1a0ceb27c93d949478d9e
David Deeley <dadeeley67@gmail.com>
2016-09-14 16:46:10 -0400

    Now prints out statements to alert user what is happening

--- Wahji-dev/setup/wahjiInit.py
+++ Wahji-dev/setup/wahjiInit.py
@@ -4,17 +4,27 @@ import os, platform
 
 def make():
 
+
 	"""creates theme folder and its contents"""
+	print "Creating content folder"
 	os.mkdir("content")
+	print "Creating themes folder"
 	os.mkdir("themes")
+	print "Creating CSS folder"
 	os.mkdir("themes/css")
+	print "Creating img folder"
 	os.mkdir("themes/img")
+	print "Creating js folder"
 	os.mkdir("themes/js")
 
 	"""creates hidden wahji file"""
+	print "Creating .wahji file"
 	file = open(".wahji","w")
 	file.close()
 
 	"""creates 404.html file"""
+	print "Creating 404.html file"
 	file = open("404.html","w+")
 	file.close()
+
+	print "Done!"
