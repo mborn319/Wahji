711132afc2f8137a76546d27f4d07924151515f3
happelj <happelj@sunyit.edu>
2016-09-13 23:21:28 -0400

    Modified yamlParser.py so that it can be used in wahji.py

--- YamlParser/yamlParser.py
+++ YamlParser/yamlParser.py
@@ -1,27 +1,29 @@
+"""
+    wahji build
+    This section generates variables to be used in static HTML files
+"""
+
+# FIRST: init the library needed for parsing Yaml
 import yaml
-"""This file opens the wahji.yml file, parses the site_name and theme,
-   then outputs the corresponding values into an html file"""
+
+"""
+    Main function for parsing working variables from Yaml file.
+    This can be pulled into another python script as a module.
+"""
 def main():
     stream = file('wahji.yml', 'r')
 
-    """Converts the contents of wahji.yml into a python dictionary"""
+    # Converts the contents of wahji.yml into a python dictionary
     site_info = yaml.load(stream) 
 
-    """Iterates over the dictionary, assigning dictionary values to associated variables"""
+    # Iterates over the dictionary, assigning dictionary values to associated variables
     for key, value in site_info.items():
         if key == "site_name":
             title = value
         if key == "theme":
             site_theme = value
 
-    """Creates output.html file and writes to it using variables extracted from wahji.yml"""
-    f2 = open('output.html', 'w')
-    ## TO DO:
-    ## Finish html header
-    f2.write("<title>" + title + "</title> \n")
-    f2.write("<h1 class=\"" + site_theme + "\">Some Sort of Head Goes Here</h1>")
-    ## TO DO:
-    ## Finish html body
-    f2.close()
+    # Creates title_tag based on parsed data from the wahji.yml
+    title_tag = "<title>" + title + "</title> \n"
 if __name__ == "__main__":
     main()
