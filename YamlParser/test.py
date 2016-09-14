1c2d3403339960554e5f39d8e4c31c342916757e
jhappel <happelj@sunyit.edu>
2016-09-14 17:36:26 -0400

    Modified yamlParser.py so that it can be used inside another python script

---
+++ YamlParser/test.py
@@ -0,0 +1,6 @@
+import yamlParser
+
+title_tag, site_theme = yamlParser.yamlParse()
+
+print title_tag
+print site_theme
--- YamlParser/yamlParser.py
+++ YamlParser/yamlParser.py
@@ -10,20 +10,17 @@ import yaml
     Main function for parsing working variables from Yaml file.
     This can be pulled into another python script as a module.
 """
-def main():
+def yamlParse():
     stream = file('wahji.yml', 'r')
 
     # Converts the contents of wahji.yml into a python dictionary
     site_info = yaml.load(stream) 
-
     # Iterates over the dictionary, assigning dictionary values to associated variables
     for key, value in site_info.items():
         if key == "site_name":
             title = value
         if key == "theme":
             site_theme = value
-
     # Creates title_tag based on parsed data from the wahji.yml
     title_tag = "<title>" + title + "</title> \n"
-if __name__ == "__main__":
-    main()
+    return title_tag, site_theme
