60dd36508a31c9a2718930dc66a5caac4b5615a8
Michael Born <mborn319@gmail.com>
2016-09-18 22:17:07 -0400

    Todo comment in shell file.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+# TODO: have the $PWD variable get sent to the wahji.py module
+#       so that we always know the current working directory.
 echo python $PWD/wahji.py \$@ >> wahji.sh
 mv wahji.sh wahji #rename script file
 chmod +x wahji #make script file executable
