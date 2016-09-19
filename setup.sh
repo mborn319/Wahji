f72760bafbf65d83bf195cb0684e53bd23ad41c2
Michael Born <mborn319@gmail.com>
2016-09-19 00:11:57 -0400

    Comment change

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 # TODO: have the $PWD variable get sent to the wahji.py module
 #       so that we always know where Wahji is installed.
-#       This will be useful when copying themes folders, default content folders, etc.
 echo python $PWD/wahji.py \$@ >> wahji.sh
 mv wahji.sh wahji #rename script file
 chmod +x wahji #make script file executable
