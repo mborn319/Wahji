583e18c1ff6c0db74e590de40a4f51410ef47554
Michael Born <mborn319@gmail.com>
2016-09-18 23:58:17 -0400

    Fixed the reference to site_settings['theme']

--- newWahji.py
+++ newWahji.py
@@ -8,13 +8,17 @@ def site(project_name, wahji_dir):
     os.mkdir(project_name)
     os.chdir(project_name)
 
+    # Copy the default site settings file into the new site dir
+    shutil.copyfile(wahji_dir + "/wahji.yml",site_dir + "/wahji.yml")
+
     # Copy the default content from the Wahji installation dir to the new site dir
-    shutil.copytree(wahji_dir + "/content", project_name + "content")
+    shutil.copytree(wahji_dir + "/content", site_dir + "/content")
 
     # Now that we have a site-specific wahji.yml settings file, read it!
     site_settings = yamlParser.yamlParse(site_dir)
 
-    print site_theme
+    print "Site settings:"
+    print site_settings
 
     # Copy the default theme from the Wahji installation dir to the new site dir
-    shutil.copytree(wahji_dir + "/themes/" + site_settings.theme,"themes")
+    shutil.copytree(wahji_dir + "/themes/" + site_settings["theme"], site_dir + "/themes")
