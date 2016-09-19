f78c8d61f8c134edfd7b4fe15ac43bc4a4683e96
Michael Born <mborn319@gmail.com>
2016-09-18 22:27:25 -0400

    Whups, changed to multi-line comments instead!

--- findWahji.py
+++ findWahji.py
@@ -1,23 +1,24 @@
 import os, platform
 
 def find():
+        lookfor = ".wahji"
 	my_platform = platform.system()
 
 	if my_platform == "Windows":
 		"""Some code"""
 	elif my_platform == "Linux":
-		lookfor = ".wahji"
-                # Thoughts: could we just traverse up through the current working directory
-                # (NOT necessarily the installed directory!) to find the .wahji file?
-                # This is better than trying to guess where the .wahji file is,
-                # ESPECIALLY since recursively searching file by file can be really slow.
+                """
+                    Thoughts: could we just traverse up through the current working directory
+                    (NOT necessarily the installed directory!) to find the .wahji file?
+                    This is better than trying to guess where the .wahji file is,
+                    ESPECIALLY since recursively searching file by file can be really slow.
+                """
 		for root, dirs, files in os.walk('/home'):
     			if lookfor in files:
 					loc = root
 					return loc
         				break
 	elif my_platform == "Darwin":
-		lookfor = ".wahji"
 		for root, dirs, files in os.walk('/Users'):
     			if lookfor in files:
 					loc = root
