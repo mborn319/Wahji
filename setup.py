39ef718d4a218ea981847a7e494a154c5055fb3e
Tea <tdrincic24@gmail.com>
2016-09-19 16:42:13 -0400

    Restructured directory

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1a',
+    version='0.0.1b',
 
     description='Static Site Generator',
     long_description='Static site generator...',
