a7bb553f213f0f3ef77df230e63207ffebe011ed
Michael Born <mborn319@gmail.com>
2016-09-18 22:25:50 -0400

    One-line comments use hash symbol

--- removeWahji.py
+++ removeWahji.py
@@ -1,21 +1,21 @@
-#deletes wahji content
+# deletes wahji content
 import os, shutil, platform
 
 def rem(loc):
-
+        
 	os.chdir(loc)
 	
 	print "deleting content"
 		
-	"""delete them folder and its contents"""
+	# delete them folder and its contents
 	shutil.rmtree("themes")
 
-	"""delete .wahji file"""
+	# delete .wahji file
 	os.remove(".wahji")
 
-	"""delete 4040.html file"""
+	# delete 404.html file
 	os.remove("404.html")
 
-	"""delete content folder"""
+	# delete content folder
 	shutil.rmtree("content")
 
